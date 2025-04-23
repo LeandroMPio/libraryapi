5c0f72ca0298aca985e7b9e7978bae467ce184a8
Leandro Martins Pio <leandro_martins_pio@atacadao.com.br>
2025-04-23 12:35:10 -0300

    feat(multi): :sparkles: -> Adicionado Auditoria JPA

--- src/main/java/com/example/libraryapi/model/Autor.java
+++ src/main/java/com/example/libraryapi/model/Autor.java
@@ -4,8 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,7 +17,8 @@ import java.util.UUID;
 @Table(name = "autor")
 @Getter
 @Setter
-@ToString
+@ToString(exclude = {"livros"})
+@EntityListeners(AuditingEntityListener.class)
 public class Autor {
     @Id
     @Column(name = "id", nullable = false)
@@ -28,6 +33,17 @@ public class Autor {
 
     @Column(name = "nacionalidade", length = 50, nullable = false)
     private String nacionalidade;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuario;
 
     @OneToMany(mappedBy = "autor")
               //(mappedBy = "autor", cascade = CascadeType.ALL)
