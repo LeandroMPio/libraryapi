5f987b08e067a085e0005dcdd04b1c2d98feaaf9
Leandro Martins Pio <leandro_martins_pio@atacadao.com.br>
2025-04-24 17:28:32 -0300

    feat(multi): :sparkles: -> Adicionado metodo para obter livro por ID

--- src/main/java/com/example/libraryapi/controller/LivroController.java
+++ src/main/java/com/example/libraryapi/controller/LivroController.java
@@ -2,18 +2,17 @@ package com.example.libraryapi.controller;
 
 import com.example.libraryapi.GenericController;
 import com.example.libraryapi.controller.dto.CadastroLivroDTO;
+import com.example.libraryapi.controller.dto.ResultadoPesquisaLivroDTO;
 import com.example.libraryapi.controller.mappers.LivroMapper;
 import com.example.libraryapi.model.Livro;
 import com.example.libraryapi.service.LivroService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("livros")
@@ -30,5 +29,14 @@ public class LivroController implements GenericController {
         URI location = gerarHeaderLocation(livro.getId());
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ResultadoPesquisaLivroDTO> obterDetalhes(@PathVariable String id) {
+        return livroService.obterPorId(UUID.fromString(id))
+                .map(livro -> {
+                    ResultadoPesquisaLivroDTO dto = livroMapper.toDTO(livro);
+                    return ResponseEntity.ok(dto);
+                }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
