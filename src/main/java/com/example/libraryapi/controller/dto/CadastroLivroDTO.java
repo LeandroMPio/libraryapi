3478faf33c7894d98a75b065ab3e67f3cf6eab04
Leandro Martins Pio <leandro_martins_pio@atacadao.com.br>
2025-04-24 12:16:58 -0300

    feat(single): :sparkles: -> Adicionado Validações em CadastroLivroDTO

--- src/main/java/com/example/libraryapi/controller/dto/CadastroLivroDTO.java
+++ src/main/java/com/example/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -1,16 +1,30 @@
 package com.example.libraryapi.controller.dto;
 
 import com.example.libraryapi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record CadastroLivroDTO(
+        @ISBN
+        @NotBlank(message = "O campo ISBN é obrigatório")
         String isbn,
+
+        @NotBlank(message = "O campo título é obrigatório")
         String titulo,
+
+        @NotNull(message = "O campo data de publicação é obrigatório")
+        @Past(message = "O campo data de publicação deve ser uma data no passado")
         LocalDate dataPublicacao,
+
         GeneroLivro genero,
         BigDecimal preco,
+
+        @NotNull(message = "O campo autor é obrigatório")
         UUID idAutor) {
 }
