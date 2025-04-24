b9a724ac56ccaef947678c7dfa4e26e48d3bf7f4
Leandro Martins Pio <leandro_martins_pio@atacadao.com.br>
2025-04-24 11:24:04 -0300

    feat(multi): :sparkles: -> Adicionado Validação com Bean Validation

--- src/main/java/com/example/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/com/example/libraryapi/controller/dto/AutorDTO.java
@@ -1,14 +1,19 @@
 package com.example.libraryapi.controller.dto;
 
 import com.example.libraryapi.model.Autor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record AutorDTO(
         UUID id,
+        @NotBlank(message = "O campo nome é obrigatório")
         String nome,
+        @NotNull(message = "O campo data de nascimento é obrigatório")
         LocalDate dataNascimento,
+        @NotBlank(message = "O campo nacionalidade é obrigatório")
         String nacionalidade) {
 
     public Autor mapearParaAutor() {
