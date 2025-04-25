7f60ba6b8a0eee2e6057d12791a0c0258422cff4
Leandro Martins Pio <leandro_martins_pio@atacadao.com.br>
2025-04-25 16:08:23 -0300

    feat(single): :sparkles: -> Adicionado usuario service

---
+++ src/main/java/com/example/libraryapi/service/UsuarioService.java
@@ -0,0 +1,15 @@
+package com.example.libraryapi.service;
+
+import com.example.libraryapi.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+    
+}
