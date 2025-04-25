b6504a950715ff7b5eccdbf64e851324703ad764
Leandro Martins Pio <leandro_martins_pio@atacadao.com.br>
2025-04-25 09:55:38 -0300

    feat(single): :sparkles: -> Adicionado DataLoader para popular banco de dev

---
+++ src/main/java/com/example/libraryapi/devinit/DataLoader.java
@@ -0,0 +1,58 @@
+package com.example.libraryapi.devinit;
+
+import com.example.libraryapi.model.Autor;
+import com.example.libraryapi.model.GeneroLivro;
+import com.example.libraryapi.model.Livro;
+import com.example.libraryapi.repository.AutorRepository;
+import com.example.libraryapi.repository.LivroRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.UUID;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+
+    private final AutorRepository autorRepository;
+    private final LivroRepository livroRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Autor autor1 = new Autor();
+        autor1.setId(UUID.fromString("0041fdbc-b415-41a0-8e60-d538dadc13d0"));
+        autor1.setNome("Maria");
+        autor1.setDataNascimento(LocalDate.of(1960, 01, 01));
+        autor1.setNacionalidade("Brasileira");
+
+        Autor autor2 = new Autor();
+        autor2.setId(UUID.fromString("3cf56cf3-0065-4d8d-b934-0d8ca0bc786f"));
+        autor2.setNome("Josefina");
+        autor2.setDataNascimento(LocalDate.of(1970, 01, 01));
+        autor2.setNacionalidade("Americana");
+
+        autorRepository.saveAll(Arrays.asList(autor1, autor2));
+
+        Livro livro1 = new Livro();
+        livro1.setIsbn("978-1585424337");
+        livro1.setTitulo("As viagens");
+        livro1.setDataPublicacao(LocalDate.of(1990, 01, 01));
+        livro1.setGenero(GeneroLivro.FICCAO);
+        livro1.setPreco(BigDecimal.valueOf(100.00));
+        livro1.setAutor(autor1);
+
+        Livro livro2 = new Livro();
+        livro2.setIsbn("978-1585424337");
+        livro2.setTitulo("As viagens");
+        livro2.setDataPublicacao(LocalDate.of(1965, 01, 01));
+        livro2.setGenero(GeneroLivro.MISTERIO);
+        livro2.setPreco(BigDecimal.valueOf(150.00));
+        livro2.setAutor(autor2);
+
+        livroRepository.saveAll(Arrays.asList(livro1, livro2));
+    }
+}
