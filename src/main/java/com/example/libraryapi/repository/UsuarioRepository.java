20aecdeb4b476b1fcd592594ee934e8477dafccc
Leandro Martins Pio <leandro_martins_pio@atacadao.com.br>
2025-04-25 16:08:09 -0300

    feat(single): :sparkles: -> Adicionado usuario repository

---
+++ src/main/java/com/example/libraryapi/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.libraryapi.repository;
+
+import com.example.libraryapi.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+}
