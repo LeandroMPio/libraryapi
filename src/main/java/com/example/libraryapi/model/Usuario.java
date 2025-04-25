faa59df4e85b71a5350bef79a1fef8eac98cea0c
Leandro Martins Pio <leandro_martins_pio@atacadao.com.br>
2025-04-25 16:04:02 -0300

    feat(single): :sparkles: -> Adicionado entidade Usuario

---
+++ src/main/java/com/example/libraryapi/model/Usuario.java
@@ -0,0 +1,30 @@
+package com.example.libraryapi.model;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID id;
+
+    @Column(length = 20, nullable = false)
+    private String login;
+
+    @Column(length = 300, nullable = false)
+    private String senha;
+
+    @Type(ListArrayType.class)
+    @Column(name = "roles", columnDefinition = "varchar(255) array")
+    private List<String> roles;
+}
