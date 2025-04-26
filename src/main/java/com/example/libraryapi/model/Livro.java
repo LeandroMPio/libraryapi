740d67bc7cc23ae2a90fe932e9565dd4ec21405f
Leandro Pio <leandropio@gmail.com>
2025-04-26 18:15:10 -0300

    refactor(multi): :recycle: -> Adicionado relacionamento com Usuario

--- src/main/java/com/example/libraryapi/model/Livro.java
+++ src/main/java/com/example/libraryapi/model/Livro.java
@@ -46,8 +46,9 @@ public class Livro {
     @Column(name = "data_atualizacao")
     private LocalDateTime dataAtualizacao;
 
-    @Column(name = "id_usuario")
-    private UUID idUsuario;
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario Usuario;
 
     @ManyToOne//(cascade = CascadeType.ALL)
             (fetch = FetchType.LAZY)
