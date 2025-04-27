2e3f34660f64f6eb760fd387a2efdc26ebe510c1
Leandro Pio <leandropio@gmail.com>
2025-04-27 17:18:41 -0300

    feat(single): :sparkles: -> Adicionado custom authentication provider

--- src/main/java/com/example/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/com/example/libraryapi/config/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder(10);
     }
 
-    @Bean
+//    @Bean
     public UserDetailsService userDetailsService(UsuarioService usuarioService) {
 
 //        UserDetails user1 = User.builder()
