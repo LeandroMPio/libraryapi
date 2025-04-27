d44222821d607704d27d502398d90f8749739f3e
Leandro Pio <leandropio@gmail.com>
2025-04-27 17:33:54 -0300

    refactor(single): :recycle: -> Alterado securityService para utilizar o custom authentication

--- src/main/java/com/example/libraryapi/security/SecurityService.java
+++ src/main/java/com/example/libraryapi/security/SecurityService.java
@@ -3,6 +3,7 @@ package com.example.libraryapi.security;
 import com.example.libraryapi.model.Usuario;
 import com.example.libraryapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +14,16 @@ public class SecurityService {
     private final UsuarioService usuarioService;
 
     public Usuario obterUsuarioLogado() {
-        String login = SecurityContextHolder.getContext().getAuthentication().getName();
-        return usuarioService.obterPorLogin(login);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication instanceof CustomAuthentication customAuthentication) {
+            return customAuthentication.getUsuario();
+        }
+
+        return null;
+
+//        String login = SecurityContextHolder.getContext().getAuthentication().getName();
+//        return usuarioService.obterPorLogin(login);
 
 //        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 //        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
