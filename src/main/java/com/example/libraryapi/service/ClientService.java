fd032f6720157183dd944047be1264a39849f4a0
Leandro Martins Pio <leandro_martins_pio@atacadao.com.br>
2025-04-28 10:10:05 -0300

    refactor(multi): :recycle: -> Adicionado salvar a senha criptografada

--- src/main/java/com/example/libraryapi/service/ClientService.java
+++ src/main/java/com/example/libraryapi/service/ClientService.java
@@ -3,6 +3,7 @@ package com.example.libraryapi.service;
 import com.example.libraryapi.model.Client;
 import com.example.libraryapi.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,11 @@ import org.springframework.stereotype.Service;
 public class ClientService {
 
     private final ClientRepository clientRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Client salvar(Client client) {
+        String senhaCriptografada = passwordEncoder.encode(client.getClientSecret());
+        client.setClientSecret(senhaCriptografada);
         return clientRepository.save(client);
     }
 
