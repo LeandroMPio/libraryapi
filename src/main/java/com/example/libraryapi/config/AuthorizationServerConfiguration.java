2eed035751027ceb32043fbbdca2333902054133
Leandro Martins Pio <leandro_martins_pio@atacadao.com.br>
2025-04-28 10:44:05 -0300

    feat(multi): :sparkles: -> Adicionado configuração inicial do authorization server

--- src/main/java/com/example/libraryapi/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/example/libraryapi/config/AuthorizationServerConfiguration.java
@@ -2,16 +2,67 @@ package com.example.libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.time.Duration;
 
 @Configuration
 @EnableWebSecurity
 public class AuthorizationServerConfiguration {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authSeverSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
+
+        httpSecurity.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
+
+        httpSecurity.oauth2ResourceServer(
+                oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt(Customizer.withDefaults()));
+
+        httpSecurity.formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(UsuarioService usuarioService) {
+//
+//        UserDetails user1 = User.builder()
+//                .username("usuario")
+//                .password(encoder.encode("123"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails user2 = User.builder()
+//                .username("admin")
+//                .password(encoder.encode("123"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+//
+//        return new CustomUserDetailsService(usuarioService);
+//    }
 
     @Bean
     public TokenSettings tokenSettings() {
